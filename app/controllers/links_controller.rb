eb38858f0415ba8794fb71634a50ae728f446bb7
Brian Heim <bheim67@gmail.com>
2017-01-09 18:13:30 -0700

    inital push

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,16 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.where("created_at > ?", Time.now - (24 * 60 * 60)).order(read_count: :desc).limit(10)
+  end
+
+  def create
+    link = Link.find_or_create_by(link_params)
+      link.read_count += 1
+      link.save
+  end
+
+  private
+    def link_params
+      params.permit(:title, :url)
+    end
+end

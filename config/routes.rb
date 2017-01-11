b553b18d9facd63e89800d5203af180bdb74e0db
Brian Heim <bheim67@gmail.com>
2017-01-10 21:45:56 -0700

    add route for api controller index to serve urlockbox

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "links#index"
 
- get '/add_read', to: 'links#create'
+  get '/add_read', to: 'links#create'
+
+  namespace :api do
+     namespace :v1 do
+       resources :links, only: [:index]
+     end
+   end
 end
